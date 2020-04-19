c406392b9f1f4a1e2e6489652cfe9a13abb89137
DmitriyValetov <dmitriy.valetov@phystech.edu>
2020-04-19 20:50:19 +0300

    initial commit

---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+
+
+class ABAE(torch.nn.Module):
+    def __init__(self, word_num, aspects_num=14, emb_seize=200):
+        super(ABAE, self).__init__()
+        self.emb_seize = emb_seize
+        self.word_num = word_num
+        self.aspects_num = aspects_num
+        self.w_embs = torch.nn.Embedding(word_num, emb_seize)
+        self.a_embs = torch.randn(aspects_num, emb_seize, requires_grad=True)
+        self.attention_M = torch.randn(emb_seize, emb_seize, requires_grad=True)
+        self.sentence_aspects = torch.nn.Linear(emb_seize, aspects_num)
+        
+    def make_sentence(self, embs):
+        """
+        input: embedded words
+        output: weights
+        """
+        ys = torch.mean(embs, axis=0).unsqueeze(dim=1)
+        di = torch.mm(embs, torch.mm(self.attention_M, ys))
+        ai = torch.nn.functional.softmax(di, dim=1)
+        zs = torch.mm(embs.T, ai)
+        return zs
+        
+    def make_sentence_raw(self, embs):
+        return torch.mean(embs, axis=0).unsqueeze(dim=1)
+        
+    def forward(self, ps, ns_list):
+        """
+        ps - positive sequence. Tensor of shape (sentence_length,)
+        ns_list - list of negaitve sequence. Tensor of shape [(sentence_length,), ...]
+        """
+        embs = self.w_embs(ps) # (sentence_length, emb_dim)
+        zs = self.make_sentence(embs) # sentence from attentioned words vectors
+        pt = torch.nn.functional.softmax(self.sentence_aspects(zs.T), dim=1).T
+        rs = torch.mm(self.a_embs.T, pt) # sentence reconstraction by aspects vectors
+        return rs, [self.make_sentence_raw(self.w_embs(ns)) for ns in ns_list]
