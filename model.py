ad47386f81d23526a11fd3250f05e3bac25b9a48
DmitriyValetov <dmitriy.valetov@phystech.edu>
2020-04-21 00:35:25 +0300

    done. it is trainable. aspects-words similarities are calculated in notebook

--- model.py
+++ model.py
@@ -2,15 +2,15 @@ import torch
 
 
 class ABAE(torch.nn.Module):
-    def __init__(self, word_num, aspects_num=14, emb_seize=200):
+    def __init__(self, word_num, aspects_num=14, emb_size=200):
         super(ABAE, self).__init__()
-        self.emb_seize = emb_seize
+        self.emb_size = emb_size
         self.word_num = word_num
         self.aspects_num = aspects_num
-        self.w_embs = torch.nn.Embedding(word_num, emb_seize)
-        self.a_embs = torch.randn(aspects_num, emb_seize, requires_grad=True)
-        self.attention_M = torch.randn(emb_seize, emb_seize, requires_grad=True)
-        self.sentence_aspects = torch.nn.Linear(emb_seize, aspects_num)
+        self.w_embs = torch.nn.Embedding(word_num, emb_size)
+        self.a_embs = torch.randn(aspects_num, emb_size, requires_grad=True)
+        self.attention_M = torch.randn(emb_size, emb_size, requires_grad=True)
+        self.sentence_aspects = torch.nn.Linear(emb_size, aspects_num)
         
     def make_sentence(self, embs):
         """
@@ -35,4 +35,4 @@ class ABAE(torch.nn.Module):
         zs = self.make_sentence(embs) # sentence from attentioned words vectors
         pt = torch.nn.functional.softmax(self.sentence_aspects(zs.T), dim=1).T
         rs = torch.mm(self.a_embs.T, pt) # sentence reconstraction by aspects vectors
-        return rs, [self.make_sentence_raw(self.w_embs(ns)) for ns in ns_list]
+        return rs, zs, [self.make_sentence_raw(self.w_embs(ns)) for ns in ns_list]
